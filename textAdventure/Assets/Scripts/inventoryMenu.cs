4ed0e735c96f54be63a6cce74e5eb759b913be96
Zurkiba <Zurkiba@gmail.com>
2014-08-01 15:51:14 -0400

    initial

---
+++ textAdventure/Assets/Scripts/inventoryMenu.cs
@@ -0,0 +1,13 @@
+	//INVENTORY STUFF
+	public Sprite item1;
+	public string item1text;
+	public Sprite item2;
+	public string item2text;
+	public Sprite item3;
+	public string item3text;
+	public Sprite item4;
+	public string item4text;
+	public Sprite item5;
+	public string item5text;
+	public Sprite item6;
+	public string item6text;
